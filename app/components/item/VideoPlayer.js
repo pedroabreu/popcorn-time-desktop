86b34aa5f38fd00c41758edc2c2595e7a39128d1
Pedro Abreu <pmig.abreu@gmail.com>
2019-05-06 13:59:23 +0100

    No need for refs

--- app/components/item/VideoPlayer.js
+++ app/components/item/VideoPlayer.js
@@ -1,47 +1,53 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Plyr from '@amilajack/react-plyr';
+import Player from '../../api/Player';
 
 type Props = {
   captions: Object,
   url: string,
   item: Object,
-  onClose: Function,
-  forwardedRef: Function
+  onClose: Function
 };
 
-export default class VideoPlayer extends Component<Props> {
-  render() {
-    const { captions, url, item, onClose, forwardedRef } = this.props;
+const VideoPlayer = function VideoPlayer({
+  captions,
+  url,
+  item,
+  onClose
+}: Props) {
+  const playerRef = useRef(null);
+  useEffect(() => Player.setPlayer(playerRef.current), []);
 
-    return (
-      <React.Fragment>
-        <Plyr
-          captions={captions}
-          type="video"
-          url={url}
-          poster={(item && item.images && item.images.fanart.full) || ''}
-          title={item.title || ''}
-          volume={10}
-          onEnterFullscreen={() => {
-            document.querySelector('.plyr').style.height = '100%';
-          }}
-          onExitFullscreen={() => {
-            document.querySelector('.plyr').style.height = '0px';
-          }}
-          ref={forwardedRef}
-        />
+  return (
+    <React.Fragment>
+      <Plyr
+        captions={captions}
+        type="video"
+        url={url}
+        poster={(item && item.images && item.images.fanart.full) || ''}
+        title={item.title || ''}
+        volume={10}
+        onEnterFullscreen={() => {
+          document.querySelector('.plyr').style.height = '100%';
+        }}
+        onExitFullscreen={() => {
+          document.querySelector('.plyr').style.height = '0px';
+        }}
+        ref={playerRef}
+      />
 
-        {url && (
-          <span
-            data-e2e="close-player"
-            role="presentation"
-            id="close-button"
-            onClick={onClose}
-          >
-            <i className="ion-md-close" />
-          </span>
-        )}
-      </React.Fragment>
-    );
-  }
-}
+      {url && (
+        <span
+          data-e2e="close-player"
+          role="presentation"
+          id="close-button"
+          onClick={onClose}
+        >
+          <i className="ion-md-close" />
+        </span>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default VideoPlayer;
